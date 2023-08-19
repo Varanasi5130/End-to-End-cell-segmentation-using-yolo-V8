88d3645911824fa4bd34f2aca5c6e9ab86c0b40f
Dhiraj <vdhiraj880@gmail.com>
2023-08-19 08:40:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+name ="cellSegmentation",
+version= '0.0.0',
+author= 'Boktiar Ahmed Bappy',
+author_email= 'vdhiraj880@gmail.com',
+packages =find_packages(),
+install_requires = []
+ )
